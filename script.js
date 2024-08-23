685299b1abf1b82152026d64edfb6069da3a3e16
marlon <106351131+MarlonKruchinski@users.noreply.github.com>
2024-08-23 08:08:53 -0300

    yjgdr

--- script.js
+++ script.js
@@ -43,20 +43,44 @@ function mostraAlternativas(){
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa;
         caixaAlternativas.appendChild(botaoAlternativas);
-
     }
-        
-    let atual = 0;
-    let perguntaAtual;
-    let historiaFinal = "";
-     
-    function mostraPergunta() {
-       if(atual >= perguntas.length){
-          mostraResultado();
-          return;
-       }
-   perguntaAtual = perguntas[atual];
-   caixaPerguntas.textContent = perguntaAtual.enunciado;
-   caixaAlternativas.textContent = "";
-   textoResultado.textContent = "";
-   mostraAlternativas();
+];
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    if(atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    textoResultado.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener('click', ()=> respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);        
+    }
+}
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacao = opcaoSelecionada.afirmacao;
+    historiaFinal = afirmacao;
+    atual++
+    mostraPergunta();
+}
+
+mostraPergunta();
+
+function mostraResultado(){
+    caixaPerguntas.textContent = "Em 2030...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
